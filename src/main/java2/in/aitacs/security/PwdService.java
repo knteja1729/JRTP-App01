9c75f47cb69ce402daf4f12974430f23e1b64931
Narendra teja <narendrateja@aitacs.com>
2022-04-07 10:58:26 +0530

    first commit

---
+++ src/main/java2/in/aitacs/security/PwdService.java
@@ -0,0 +1,29 @@
+package in.aitacs.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PwdService {
+	
+	public static String encoder(String txt) {
+		Encoder encoder = Base64.getEncoder();
+		byte[] encode = encoder.encode(txt.getBytes());
+		return new String(encode);
+	}
+	
+	public static String decode(String encoder) {
+		Decoder decoder = Base64.getDecoder();
+		System.out.println("Iam second version");
+		byte[] encode = decoder.decode(encoder);
+		return new String(encode);
+	}
+	
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * String encoder = encoder("Narendra"); System.out.println(encoder); String
+	 * decode = decode(encoder); System.out.println(decode); }
+	 */
+
+}
